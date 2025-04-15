f05f7843afe2d82d50028810ffc23f32f21ebe7c
mnshrstg05 <rstgmnsh05@gmail.com>
2025-04-15 11:34:15 +0530

    Dashboard inventory reports  changes

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { FaBox, FaTags } from 'react-icons/fa';
-import Inventory from '../../../backend/models/InventoryModel';
+
 
 const Dashboard = () => {
     const [totalProducts, setTotalProducts] = useState(() => Number(localStorage.getItem('totalProducts')) || 0);
